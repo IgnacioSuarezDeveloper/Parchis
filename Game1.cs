6a99e521b1d530a6d516bc219661988f1ea0ed42
Camila <vivascamila555@gmail.com>
2026-03-29 20:54:29 +0200

    v1 dibujando tablero y fichas

---
+++ Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PARCHIS
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 1000;
+            _graphics.PreferredBackBufferHeight = 1000;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            SpritesToTexture.LoadFullSprite(Content);           
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+
+            //dibujando la imagen.
+            SpritesToTexture.DrawBoard(_spriteBatch, 10, 10);
+
+            //dibujando ficha.
+            SpritesToTexture.DrawRedChip(_spriteBatch,100,100);
+
+
+            
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SpritesToTexture.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PARCHIS
+{
+    internal static class SpritesToTexture
+    {
+       static Texture2D fullSprite;
+
+        //cargando la imagen completa.
+        public static void LoadFullSprite(ContentManager Content)
+        {
+            fullSprite = Content.Load<Texture2D>("sprite.png");
+        }
+
+        //dibujando todo el sprite.
+        public static void DrawFullSprite(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(fullSprite, new Vector2(0, 0), null, Color.White, 0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
+        }
+
+        //dibuja Ficha roja.
+        public static void DrawRedChip(SpriteBatch _spriteBatch, float x, float y)
+        {
+            // Definimos qué parte del sprite queremos (x, y, ancho, alto)
+            Rectangle fuente = new Rectangle(0, 0, 200, 250);
+
+            // Dibujamos
+            _spriteBatch.Draw(
+                fullSprite,
+                new Rectangle((int)x,(int)y,50,50),
+                fuente,
+                Color.White
+            );
+        }
+
+        //dibuja Tablero.
+        public static void DrawBoard(SpriteBatch _spriteBatch, float x,float y)
+        {
+            // Definimos qué parte del sprite queremos (x, y, ancho, alto)
+            Rectangle fuente = new Rectangle(861, 360, 600, 600);
+
+            // Dibujamos
+            _spriteBatch.Draw(
+                fullSprite,
+                new Rectangle((int)x, (int)y, 1000, 1000),
+                fuente,
+                Color.White
+            );
+        }
+    }
+}
